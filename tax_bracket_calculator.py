336544e142aeb7fbf941d6308968e647e30ca467
benryan03 <50218096+benryan03@users.noreply.github.com>
2020-02-04 13:07:35 -0500

    Add files via upload

--- tax_bracket_calculator.py
+++ tax_bracket_calculator.py
@@ -1,22 +1,32 @@
-#Tax Bracket Calculator 1.0
-print("Tax Bracket Calculator");
+#Tax Bracket Calculator 1.1
+print("Tax Bracket Calculator 1.1");
 
-bracket_1_limit = int(input("Enter the limit of bracket 1 (lowest bracket): "));
-bracket_1_percentage = float(input("Enter the percentage of bracket 1 (period followed by number): "))
-bracket_2_limit = int(input("Enter the limit of bracket 2:"));
-bracket_2_percentage = float(input("Enter the percentage of bracket 2 (period followed by number): "))
-bracket_3_limit = int(input("Enter the limit of bracket 3:"));
-bracket_3_percentage = float(input("Enter the percentage of bracket 3 (period followed by number): "))
+limits = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+rates = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+bracket_count = int(input("Enter the number of brackets (limit 10): "))
+
+for x in range (0, bracket_count):
+    print(x);
+    limits[x] = int(input("Enter the limit of bracket " + str(x+1) + ": "));
+    rates[x] = float(input("Enter the tax rate of bracket " + str(x+1) + " (period followed by number): "));
 
 income = int(input("Enter income: "));
 
-if income <= bracket_1_limit:
-    result = int(income * (1 - bracket_1_percentage));
-elif income <= bracket_2_limit:
-    remainder = income - bracket_1_limit;
-    result = int((bracket_1_limit * (1 - bracket_1_percentage)) + (remainder * (1 - bracket_2_percentage)));
-else:
-    remainder = income - (bracket_1_limit + bracket_2_limit);
-    result = int((bracket_1_limit * (1 - bracket_1_percentage)) + (bracket_2_limit * (1 - bracket_2_percentage ))+ (remainder * (1 - bracket_3_percentage)));
+ceiling = limits[0];
+
+x = 0;
+result = 0.0;
+
+while income > ceiling:
+    x = x + 1;
+    ceiling = limits[x];
+
+#calculations for initial brackets
+for y in range (0, x):
+    result = result + (limits[y] - limits[y - 1]) * (1 - rates[y]);
+
+#calculation for last bracket
+result = result + (income - limits[x - 1]) * (1 - rates[x]);
 
 print("Post tax income: " + str(result));
