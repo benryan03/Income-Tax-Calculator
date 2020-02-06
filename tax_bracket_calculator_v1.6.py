8ae2b22b118f933ed623e6909d692c1f6fc1cfde
benryan03 <50218096+benryan03@users.noreply.github.com>
2020-02-06 12:15:39 -0800

    v1.6 update

--- tax_bracket_calculator_v1.6.py
+++ tax_bracket_calculator_v1.6.py
@@ -1,5 +1,5 @@
-#Tax Bracket Calculator v1.5
-print("Tax Bracket Calculator v1.5")
+#Tax Bracket Calculator v1.6
+print("Tax Bracket Calculator v1.6")
 
 limits = []
 rates = []
@@ -9,6 +9,8 @@ bracket_count = int(input("Enter the number of brackets: "))
 for x in range (0, bracket_count):
     limits.append(int(input("Enter the limit of bracket " + str(x+1) + ": ")))
     rates.append(float(input("Enter the tax rate of bracket " + str(x+1) + " (period followed by number): ")))
+
+rates.append(float(input("Enter the tax rate for income above " + str(limits[-1]) + " (period followed by number): ")))
 
 #I don't know why the lists need to have a an extra zero at the end to work
 #I should try to fix this at some point
@@ -39,10 +41,10 @@ if income < max_bracket:
 
 else:
     #calculations for initial brackets
-    for y in range (0, max_bracket_index):
+    for y in range (0, max_bracket_index + 1):
         result = result + (limits[y] - limits[y - 1]) * (1 - rates[y])
 
     #calculation for final bracket
-    result = result + (income - limits[z - 1]) * (1 - rates[z])
+    result = result + (income - limits[z - 2]) * (1 - rates[-2])
 
 print("Net income: " + str(result))
