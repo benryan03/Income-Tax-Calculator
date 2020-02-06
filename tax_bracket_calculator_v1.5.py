55d5a465ec7dd3a20ac2b1b4228afd28c2e45ae5
benryan03 <50218096+benryan03@users.noreply.github.com>
2020-02-05 20:08:00 -0800

    v1.5 update

--- tax_bracket_calculator_v1.5.py
+++ tax_bracket_calculator_v1.5.py
@@ -1,10 +1,8 @@
-#Tax Bracket Calculator 1.4
-print("Tax Bracket Calculator 1.4")
+#Tax Bracket Calculator v1.5
+print("Tax Bracket Calculator v1.5")
 
 limits = []
 rates = []
-
-#gathering user inputs
 
 bracket_count = int(input("Enter the number of brackets: "))
 
@@ -12,29 +10,32 @@ for x in range (0, bracket_count):
     limits.append(int(input("Enter the limit of bracket " + str(x+1) + ": ")))
     rates.append(float(input("Enter the tax rate of bracket " + str(x+1) + " (period followed by number): ")))
 
+#I don't know why the lists need to have a an extra zero at the end to work
+#I should try to fix this at some point
+limits.append(0)
+rates.append(0)
+
 income = int(input("Enter income: "))
 
-#all user inputs complete
-
+ceiling = limits[0]
 max_bracket = limits[x]
 max_bracket_index = x
 result = 0.0
+z = 0
 
 if income < max_bracket:
 
     #this loop calculates how many brackets to apply
-    x = 0
-    ceiling = limits[0]
     while income > ceiling:
-        x = x + 1
-        ceiling = limits[x]
+        z = z + 1
+        ceiling = limits[z]
 
-        #calculations for initial brackets
-        for y in range (0, x):
-            result = result + (limits[y] - limits[y - 1]) * (1 - rates[y])
+    #calculations for initial brackets
+    for y in range (0, z):
+        result = result + (limits[y] - limits[y - 1]) * (1 - rates[y])
 
-        #calculation for final bracket
-        result = result + (income - limits[x - 1]) * (1 - rates[x])
+    #calculation for final bracket
+    result = result + (income - limits[z - 1]) * (1 - rates[z])
 
 else:
     #calculations for initial brackets
@@ -42,6 +43,6 @@ else:
         result = result + (limits[y] - limits[y - 1]) * (1 - rates[y])
 
     #calculation for final bracket
-    result = result + (income - limits[x - 1]) * (1 - rates[x])
+    result = result + (income - limits[z - 1]) * (1 - rates[z])
 
 print("Net income: " + str(result))
